1b92c7e795da4e80f0f8b9c6267e82ead5564c02
John5Cruz <john._l@hotmail.com>
2026-01-07 21:23:56 -0600

    Comments for functions included

--- main.py
+++ main.py
@@ -11,49 +11,30 @@ import sqlite3
 ruta = "https://www.mercadolibre.com.mx/motocicleta-chopper-italika-tc-300-negra/up/MLMU3007051693"
 
 
-def guardar_datos(date: str, price: int):
+def guardar_datos(date: str, price: int):   # save the data in a database file
     conexion = sqlite3.connect('C:/Users/john_/Documents/TrackingPrices/datos.db')
     cursor = conexion.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS motocicleta(
-    fecha TEXT,
-    precio INTEGER
-    )
-    ''')
+    cursor.execute("CREATE TABLE IF NOT EXISTS motocicleta(fecha TEXT,precio INTEGER)")
     cursor.execute("INSERT INTO motocicleta (fecha, precio) VALUES (?, ?)", (date, price))
     conexion.commit()
     conexion.close()
 
 
-def time_taken(func):
-    def wrapper():
-        t1 = time.time()
-        func()
-        t2 = time.time()
-        dt = t2 - t1
-        print(f"Este proceso requirio de {int(dt)} segundos")
-    return wrapper
-
-
-@time_taken
-def open_webpages():
+def open_webpages():    # open the webpage to get the price of the product in MercadoLibre
     tiempo = datetime.datetime.now()
     fecha = str(tiempo).split('.')[0]
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_argument("--windows-size=1280,720")
-    # options.add_argument("--headless")
-    # driver = Chrome(service=service, options=options)
     driver = ch.Chrome(options=options)
     driver.get(ruta)
     time.sleep(2)
     elemento = driver.find_element(By.CLASS_NAME, "ui-pdp-price__second-line")
     precio = elemento.text.split('\n')[1].replace(',', '')
-    print(f"Este es el precio obtenido {int(precio)} en {fecha}")
+    print(f"This is current price {int(precio)} at {fecha}")
     guardar_datos(fecha, int(precio))
     driver.quit()
 
 
 if __name__ == '__main__':
     open_webpages()
-    print("Proceso conluido")
